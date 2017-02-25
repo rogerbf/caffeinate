282ccc7c15f1ceb1ed23bd3825c2ef502782f31d
rogerbf <roger@bergfalk.nu>
2017-02-25 14:06:33 +0100

    properly handle errors

--- caffeinate.js
+++ caffeinate.js
@@ -1,21 +1,17 @@
 module.exports = ({ spawn, args }, options = {}) => {
   return new Promise((resolve, reject) => {
-    try {
-      const caffeinate = spawn(
-        `caffeinate`,
-        args
-        .addAlias({ timeout: `t` })
-        .addAlias({ watch: `w` })
-        .addAlias({ pid: `w` })(options),
-        {
-          detached: true,
-          stdio: `ignore`
-        }
-      )
-      caffeinate.unref()
-      resolve(caffeinate.pid)
-    } catch (error) {
-      reject(error)
-    }
+    const caffeinate = spawn(
+      `caffeinte`,
+      args
+      .addAlias({ timeout: `t` })
+      .addAlias({ watch: `w` })
+      .addAlias({ pid: `w` })(options),
+      {
+        detached: true,
+        stdio: `ignore`
+      }
+    ).on(`error`, reject)
+    caffeinate.unref()
+    process.nextTick(resolve.bind(null, caffeinate.pid))
   })
 }
